1402c590e84f17e273613575150624c0268103d7
ChrisPau0530 <230809@utxicotepec.edu.mx>
2025-04-01 17:40:31 -0600

    Ya agarra diseños
    Falta correcciones

--- Practicas/src/app/components/header/header.component.ts
+++ Practicas/src/app/components/header/header.component.ts
@@ -5,31 +5,14 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,RouterLink ],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  isOpen = false;
   isDropdownOpen = false;
 
-  toggleMenu() {
-    this.isOpen = !this.isOpen;
-  }
-
-  toggleDropdown() {
+  toggleDropdown(event: MouseEvent): void {
     this.isDropdownOpen = !this.isDropdownOpen;
-  }
-
-  @HostListener('document:click', ['$event'])
-  closeMenusOutside(event: Event) {
-    const targetElement = event.target as HTMLElement;
-    const isButtonClick = targetElement.closest('button');
-    const isMenuContent = targetElement.closest('.absolute');
-
-    if (!isButtonClick && !isMenuContent) {
-      this.isOpen = false;
-      this.isDropdownOpen = false;
-    }
   }
 }
